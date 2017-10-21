373b63842d0d250597c3d481327f8e02a5bef9e6
Jonathan Higgs <jonathan.higgs.11@mail.wbs.ac.uk>
2017-10-21 14:17:24 +0100

    Basic interfaces for Request/Response

---
+++ Codebase/MessageRouter/MessageRouter/IReceiver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageRouter
+{
+    /// <summary>
+    /// Interface encapsulates a connection that is able to receive and synchronously reply to incoming messages from a remote
+    /// </summary>
+    public interface IReceiver
+    {
+        /// <summary>
+        /// Gets an enumerable of <see cref="IAddress"/> that the receiver is listening to
+        /// </summary>
+        IEnumerable<IAddress> Addresses { get; }
+
+
+        /// <summary>
+        /// Starts the receiver listening for incoming messages on the specified <see cref="IAddress"/>
+        /// </summary>
+        /// <param name="address">Address to bind to</param>
+        void Bind(IAddress address);
+
+
+        /// <summary>
+        /// Stops the receiver listening for incoming messages on the specified <see cref="IAddress"/>
+        /// </summary>
+        /// <param name="address"></param>
+        void Unbind(IAddress address);
+
+
+        /// <summary>
+        /// Synchronously retrieves a <see cref="RequestTask"/>
+        /// </summary>
+        /// <returns></returns>
+        RequestTask Receive();
+    }
+}
---
+++ Codebase/MessageRouter/MessageRouter/RequestTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageRouter
+{
+    /// <summary>
+    /// Struct to combine an incoming request with a handler to return the response
+    /// </summary>
+    public struct RequestTask
+    {
+        /// <summary>
+        /// Stores a readonly reference to an incoming <see cref="Message"/>
+        /// </summary>
+        public readonly Message Request;
+
+
+        /// <summary>
+        /// Stores a readonly reference to an action that will send a response <see cref="Message"/>
+        /// </summary>
+        public readonly Action<Message> Handler;
+
+
+        /// <summary>
+        /// Initializes a new instance of a RequestTask composed of the supplied request <see cref="Message"/> and handler
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="responseAction"></param>
+        public RequestTask(Message request, Action<Message> responseAction)
+        {
+            Request = request ?? throw new ArgumentNullException(nameof(request));
+            Handler = responseAction ?? throw new ArgumentNullException(nameof(responseAction));
+        }
+    }
+}
