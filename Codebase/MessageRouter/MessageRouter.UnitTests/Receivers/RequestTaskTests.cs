5b1eca2c14d332ae98c265522f3572ffaf432a5a
Jonathan Higgs <jonathan.higgs.11@mail.wbs.ac.uk>
2017-11-30 20:37:09 +0000

    Moved RequestTaskTests

--- Codebase/MessageRouter/MessageRouter.UnitTests/Receivers/RequestTaskTests.cs
+++ Codebase/MessageRouter/MessageRouter.UnitTests/Receivers/RequestTaskTests.cs
@@ -3,7 +3,7 @@ using NUnit.Framework;
 using MessageRouter.Messages;
 using MessageRouter.Receivers;
 
-namespace MessageRouter.UnitTests
+namespace MessageRouter.UnitTests.Receivers
 {
     [TestFixture]
     public class RequestTaskTests
