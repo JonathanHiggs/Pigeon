7a6ddbd8c616c4809e3f974458193175ca69d320
Jonathan Higgs <jonathan.higgs.11@mail.wbs.ac.uk>
2019-06-27 11:02:28 +0100

    Rearrange tcp address

--- Codebase/Pigeon/Pigeon/Addresses/TcpAddress.cs
+++ Codebase/Pigeon/Pigeon/Addresses/TcpAddress.cs
@@ -22,7 +22,19 @@ namespace Pigeon.Addresses
         /// IP regex matching pattern
         /// </summary>
         public static readonly string IPPattern = @"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
-        
+
+
+        /// <summary>
+        /// Initializes a new instance of a <see cref="TcpAddress"/>
+        /// </summary>
+        /// <param name="name">Remote DNS name or IP address</param>
+        /// <param name="port">IP port number</param>
+        protected TcpAddress(string name, ushort port)
+        {
+            this.name = name;
+            this.port = port;
+        }
+
 
         /// <summary>
         /// Initializes a wildcard address
@@ -41,14 +53,14 @@ namespace Pigeon.Addresses
 
 
         /// <summary>
-        /// Initializes a new instance of a TcpAddress after checking the name is valid
+        /// Initializes a new instance of a <see cref="TcpAddress"/> after checking the name is valid
         /// </summary>
         /// <param name="name">Remote DNS name or IP address</param>
         /// <param name="port">IP port number</param>
         /// <returns></returns>
         public static TcpAddress FromNameAndPort(string name, ushort port)
         {
-            if (String.IsNullOrWhiteSpace(name))
+            if (string.IsNullOrWhiteSpace(name))
                 throw new ArgumentException("Name can not be null, empty or whitespace", nameof(name));
 
             if (!Regex.IsMatch(name, NamePattern) && !Regex.IsMatch(name, IPPattern))
@@ -59,15 +71,36 @@ namespace Pigeon.Addresses
 
 
         /// <summary>
-        /// Initializes a new instance of a TcpAddress
+        /// Determines whether the two specified <see cref="TcpAddress"/>es have the same value
         /// </summary>
-        /// <param name="name">Remote DNS name or IP address</param>
-        /// <param name="port">IP port number</param>
-        private TcpAddress(string name, ushort port)
-        {
-            this.name = name;
-            this.port = port;
-        }
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator ==(TcpAddress a, TcpAddress b) => a.name == b.name && a.port == b.port;
+
+
+        /// <summary>
+        /// Determines whether the two specified <see cref="TcpAddress"/>es have the different values
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator !=(TcpAddress a, TcpAddress b) => !a.Equals(b);
+
+
+        /// <summary>
+        /// Determines whether the supplied object is equal to this instance
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj) => !(obj is null) && obj is TcpAddress other && this == other;
+
+
+        /// <summary>
+        /// Returns the hash code for this address
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode() => ToString().GetHashCode();
 
 
         /// <summary>
@@ -75,11 +108,11 @@ namespace Pigeon.Addresses
         /// </summary>
         /// <param name="other">The address to compare to this instance</param>
         /// <returns></returns>
-        public bool Equals(IAddress other) => this.ToString() == other.ToString();
+        public bool Equals(IAddress other) => !(other is null) && other is TcpAddress otherAddress && this == otherAddress;
 
 
         /// <summary>
-        /// Returns a string that represents the address
+        /// Returns a string that represents this <see cref="TcpAddress"/>
         /// </summary>
         /// <returns>Address in string form</returns>
         public override string ToString() => $"tcp://{name}:{port}";
